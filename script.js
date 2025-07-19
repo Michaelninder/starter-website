6af82c5546a4aab92b32b7937cf08686208e8fdb
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-07-19 16:51:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const scrollToTopItem = document.getElementById('scroll-to-top');
+window.addEventListener('scroll', function(){
 
+    if (document.body.scrollTop >= 50){
+        scrollToTopItem.style.visibility = 'visible'
+    } else{
+        scrollToTopItem.style.visibility = 'hidden'
+    }
+});
+/*
+scrollToTopItem.addEventListener('click', function(){
+    document.body.scrollTop = 0;
+});
+*/
+scrollToTopItem.addEventListener('click', function(){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
